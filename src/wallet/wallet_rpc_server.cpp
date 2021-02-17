dedca2466562002371c49c6da64ea8ced4ad679b
Sayan Bhattacharyya <sohamb03@outlook.com>
2021-02-17 21:46:54 +0530

    Merge Patch: 015da03a6fb800527fec9b7997f4add772596679

--- src/wallet/wallet_rpc_server.cpp
+++ src/wallet/wallet_rpc_server.cpp
@@ -4401,7 +4401,15 @@ public:
         wal->stop();
       });
 
-      wal->refresh(wal->is_trusted_daemon());
+      try
+      {
+        wal->refresh(wal->is_trusted_daemon());
+      }
+      catch (const std::exception& e)
+      {
+        LOG_ERROR(tools::wallet_rpc_server::tr("Initial refresh failed: ") << e.what());
+      }
+      
       // if we ^C during potentially length load/refresh, there's no server loop yet
       if (quit)
       {
